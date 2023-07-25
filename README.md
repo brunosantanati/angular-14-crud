c85d49053a025dab8ae1d4f93b60f78e113df8fe
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-07-25 11:55:58 -0300

    Codigo da aula 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Angular 14: aplique os conceitos e desenvolva seu primeiro CRUD](https://cursos.alura.com.br/course/angular-explorando-framework)
+
+## Links
+
+[GitHub repo - aula 1](https://github.com/alura-cursos/2438-angular-memoteca/tree/aula-1)
---
+++ codigo-do-curso/2438-angular-memoteca-aula-1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
